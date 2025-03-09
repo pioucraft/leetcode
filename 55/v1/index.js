9c0a439b13047345580a5cfc60789a26822ea332
Nathan <hello@gougoule.ch>
2025-03-09 11:29:23 +0100

    Add canJump function to determine if you can reach the last index of an array

---
+++ 55/v1/index.js
@@ -0,0 +1,10 @@
+var canJump = function (nums) {
+    let covered = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (i > covered) return false;
+        covered = Math.max(covered, nums[i] + i);
+    }
+    return true;
+};
+
+console.log(canJump([3, 2, 1, 0, 4]));
